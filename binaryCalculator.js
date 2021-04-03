2a31de5eb011c23f2d94103c3adae9531e88021c
LeandroMLS <80489021+LeandroMLS@users.noreply.github.com>
2021-04-03 20:51:59 -0300

    first
    primeira versão da calculadora binária

---
+++ binaryCalculator.js
@@ -0,0 +1,78 @@
+let result = document.querySelector('#res')
+let operation = ""
+let firstValor = ""
+let secondValor = ""
+let answer = ""
+
+function showOnScreen(num) {
+    if (num.innerHTML == 1) {
+        result.innerHTML += "1"
+    } else {
+        result.innerHTML += "0"
+    }
+}
+
+function cleanScreen() {
+    result.innerHTML = ""
+}
+
+function setOperation(signal) {
+    let re = /^([\d+])/
+    firstValor = result.innerHTML
+    console.log(result.innerHTML)
+    switch (signal.innerHTML) {
+        case "+":
+            operation = "Sum"
+            break
+        case "-":
+            operation = "Sub"
+            break
+        case "*":
+            operation = "Mul"
+            break
+        default:
+            operation = "Div"
+    }
+    console.log(firstValor)
+    result.innerHTML += signal.innerHTML
+}
+
+function calculator() {
+    secondValor = /[\*\/\+\-](\d+)/i.exec(result.innerHTML)[1]
+
+    firstValor = firstValor.split("")
+    secondValor = secondValor.split("")
+
+    firstValor = firstValor.reverse()
+    secondValor = secondValor.reverse()
+
+    for (let i in firstValor) {firstValor[i] = firstValor[i]*(2**i)}
+    let newFirstValor = firstValor.reduce((accumulator,currentValue) => accumulator + currentValue)
+
+    for (let i in secondValor) {secondValor[i] = secondValor[i]*(2**i)}
+    let newSecondValor = secondValor.reduce((accumulator,currentValue) => accumulator + currentValue)
+
+    console.log(secondValor)
+    switch (operation) {
+        case "Sum":
+            answer = newFirstValor + newSecondValor
+            break
+        case "Sub":
+            answer = newFirstValor - newSecondValor
+            break
+        case "Mul":
+            answer = newFirstValor * newSecondValor
+            break
+        default:
+            answer = newFirstValor / newSecondValor
+    }
+    let binaryAnswer = []
+    binaryAnswer.push(parseInt(answer % 2))
+    do {
+        answer = answer / 2
+        binaryAnswer.push(parseInt(answer % 2))
+    } while (answer / 2 >= 1)
+
+    binaryAnswer = binaryAnswer.reverse().join("")
+    result.innerHTML = binaryAnswer
+}
